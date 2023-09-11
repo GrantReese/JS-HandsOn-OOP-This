1ec7bc68987aff5246b220a68f739a9f3911fe8a
IvaH1992 <119977613+GrantReese@users.noreply.github.com>
2023-09-11 13:11:37 -0500

    exercise 2.1

--- index.js
+++ index.js
@@ -3,18 +3,37 @@
 // Exercise 1.2: Add a method to the Car class
 //  called displayInfo which logs the car's make, model, and year.
 
-class Car {
-    constructor (make, model, year){
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
+// class Car {
+//     constructor (make, model, year){
+//         this.make = make;
+//         this.model = model;
+//         this.year = year;
+//     }
    
+// }
+// const jeep = new Car("jeep", "grand cherokee", 1987);
+
+// function displayInfo(Car) {
+//     console.log(this.make, this.model, this.year);
+
+//    }
+//    displayInfo(jeep);
+
+// Exercise 2.1: Craft a regular function within an 
+// object, then invoke it. Observe the behavior of "this".
+// Exercise 2.2: Create an arrow function within another 
+// object. Execute it and note the behavior of "this".
+// Exercise 2.3: Using the .bind() method, manually set
+//  the context of a function and observe the change in
+//   the behavior of "this".
+
+const user = {
+    firstName: "Trevor",
+    lastName: "Ariza",
+    fullName: function() {
+        console.log(this.firstName + " " +this.lastName)
+
+    }
+    
 }
-const jeep = new Car("jeep", "grand cherokee", 1987);
-
-function displayInfo(Car) {
-    console.log(this.make, this.model, this.year);
-
-   }
-   displayInfo(jeep);
+user.fullName();
